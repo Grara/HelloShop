1952a97e799457838994c5cefb8b83921e666be8
Grara <nomj18@gmail.com>
2022-12-15 17:28:06 +0900

    배포 테스트

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=helloshop
+
+echo "> 빌드 파일 복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 PID확인"
+
+CURRENT_PID=$(pgrep -fl helloshop-webservice | grep jar | awk'{print $1}')
+
+echo "현재 구동중인 PID : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 현재 구동중인 애플리케이션 없음"
+else
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
