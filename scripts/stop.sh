060ca6e6664e21b31dcd38705dc7d0869f43381c
Grara <nomj18@gmail.com>
2022-12-16 19:32:30 +0900

    stop.sh 오타 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,7 +7,9 @@ source ${ABSDIR}/profile.sh
 IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 PID 확인"
-IDLE_PID=$(lsof -ti tep:${IDLE_PORT})
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+echo "> IDLE_PID: ${IDLE_PID}"
 
 if [ -z ${IDLE_PID} ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료안함"
